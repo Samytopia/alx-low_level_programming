fa469845e9c7a3a4c9b17f8f211526d9afa4dc7c
Samytopia <allahnanasamson94@gmail.com>
2022-12-08 01:33:24 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- prints programming is like a multilingual puzzle
+ *
+ * return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+
+}
+
