a10e3841ee567b025e4578ca414a15477b2f9c71
Samytopia <allahnanasamson94@gmail.com>
2022-12-08 01:59:43 -0800

    Hello, pprintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main prints phrase with proper grammar
- *
+ * main- print phrase with proper grammar,
+ * but the outcome is a piece of art,
  * return 0 if no errors, otherwise non-zero
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
