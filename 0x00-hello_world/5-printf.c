5cc3834a101236358213bf42785bc8b75d766717
Samytopia <allahnanasamson94@gmail.com>
2022-12-08 01:54:40 -0800

    Hello, pprintf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main prints phrase with proper grammar
+ *
+ * return 0 if no errors, otherwise non-zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
