a7b23750decca9a14889f3bc39eea35b29f4032b
Samytopia <allahnanasamson94@gmail.com>
2023-01-04 15:09:52 -0800

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,12 @@
 
 int factorial(int n)
 {
-	if (n < 0)
-	return (-1);
-	else if (n == 0)
-	return (0);
-	else
-	return (n * factorial(n - 1));
+	if (n == 0)/*base condition*/
+	return (1);
 
+	else if (n < 0)/*base condition*/
+	return (-1);
+
+	else
+	return (n * factorial(n - 1));/*recursive call*/
 }
